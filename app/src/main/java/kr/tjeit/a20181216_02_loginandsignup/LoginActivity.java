5798737edcc9b042c4652654f903045a50bd4bf1
yorudaydream <yorudaydream@gmail.com>
2018-12-16 14:47:52 +0900

    Version 3

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
@@ -1,31 +1,43 @@
 package kr.tjeit.a20181216_02_loginandsignup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
+
+    TextView signUpTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         public abstract void setupEvent();
-        public abstract void setupValues();
+        public abstract void setValues();
         public abstract void bindViews();
     }
 
     @Override
     public void setupEvent() {
-
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public void setupValues() {
+    public void setValues() {
 
     }
 
     @Override
     public void bindViews() {
 
+        signUpTxt = findViewById(R.id.signUpTxt);
     }
 }
