12e46b99f2db2f6220e3781c8b21b00a396b9e16
yorudaydream <yorudaydream@gmail.com>
2018-12-16 15:08:06 +0900

    Version 4

---
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpStep2Activity.java
@@ -0,0 +1,32 @@
+package kr.tjeit.a20181216_02_loginandsignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpStep2Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_step2);
+
+        bindViews();
+        setupEvent();
+        setupValues();
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setupValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
